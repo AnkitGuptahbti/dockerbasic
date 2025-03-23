8082b6624b381d3bfcfbc081716ed0142b2f243b
Jaghit Singh <jaghit.singh@daffodilsw.com>
2025-03-23 09:10:55 +0530

    network and volume

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,49 @@
-# layers in dockerfile
-# under this 6 layer till line 4,7,10,14,15,16
-# base image
+# # layers in dockerfile
+# # under this 6 layer till line 4,7,10,14,15,16
+# # base image
+# FROM  node:20 
+
+# # working directory
+# WORKDIR /app
+
+# # copy every files
+# COPY . .
+
+
+# # run command to build the code
+# RUN npm install
+# RUN npm run build
+# RUN npx prisma generate
+
+# # ENV DATABASE_URL ="URL" THATS NOTACCEPTALE LEKIN OF DATA VISIVLE TO OTHER WHILEL USING THIS IMAGE
+
+# # expost port
+# EXPOSE 3000
+
+# # final command run when running the container
+# CMD ["node", "dist/index.js"]
+
+# ////////////////////////////********************************?????????????????
+# how does this can be optimise it means more layer should  be cached.
+# how often does in a project does depandecncy got changes,how odtern does npm istatll layer does changes => it will be good that we cache npm i  step considered depandecua dors not changed.
+
 FROM  node:20 
 
 # working directory
 WORKDIR /app
-
-# copy every files
-COPY . .
+# COPY package* SAME AS BELOW LINE
+COPY package.json package-lock.json ./
 
 
 # run command to build the code
 RUN npm install
+# RUN npx prisma generate
+
+
+COPY . .
+
+
 RUN npm run build
-RUN npx prisma generate
 
 # ENV DATABASE_URL ="URL" THATS NOTACCEPTALE LEKIN OF DATA VISIVLE TO OTHER WHILEL USING THIS IMAGE
 
@@ -22,6 +52,3 @@ EXPOSE 3000
 
 # final command run when running the container
 CMD ["node", "dist/index.js"]
-
-
-
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,68 @@
 
-import { PrismaClient } from "@prisma/client";
-import express from "express";
+// import { PrismaClient } from "@prisma/client";
+// import express from "express";
 
-const app = express();
+// const app = express();
+// app.use(express.json());
+
+// const client = new PrismaClient();
+
+// app.get("/", (req, res) => {
+//     res.json({
+//         message: "Healthy server"
+//     })
+// })
+
+// app.post("/", async (req, res) => {
+//     await client.user.create({
+//         data: {
+//             email: req.body.email,
+//             name: req.body.name
+//         }
+//     })
+
+//     res.json({
+//         message: "Done signing up!"
+//     })
+// })
+
+// app.listen(3000,() => {
+//     console.log("Server is running on port 3000")})
+
+
+
+import express, { Express, Request, Response } from 'express';
+import { User } from './db';
+
+const app: Express = express();
+const port: number = 3000;
+
 app.use(express.json());
 
-const client = new PrismaClient();
+// Endpoint to create a user
+app.post('/user', async (req: Request, res: Response) => {
+  const { name, age, email } = req.body;
+  const newUser = new User({ name, age, email });
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "Healthy server"
-    })
-})
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).send({ message: 'User created', user: savedUser });
+  } catch (err) {
+    res.status(500).send({ message: 'Error creating user', error: err });
+  }
+});
 
-app.post("/", async (req, res) => {
-    await client.user.create({
-        data: {
-            email: req.body.email,
-            name: req.body.name
-        }
-    })
+// Endpoint to fetch all users
+app.get('/users', async (req: Request, res: Response) => {
+  try {
+    const users = await User.find();
+    res.status(200).send(users);
+  } catch (err) {
+    res.status(500).send({ message: 'Error fetching users', error: err });
+  }
+});
 
-    res.json({
-        message: "Done signing up!"
-    })
-})
-
-app.listen(3000,() => {
-    console.log("Server is running on port 3000")})
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
